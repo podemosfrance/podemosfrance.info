bfe4526003155e6c5b60df589c3b4311ec31b4e7
Alain Rodriguez <arodrime@gmail.com>
2016-02-07 14:38:22 +0100

    Ajout nombre d'inscrits sur la sidebar

--- wp-content/themes/circulo_podemos/parts/shared/sidebar.php
+++ wp-content/themes/circulo_podemos/parts/shared/sidebar.php
@@ -1,4 +1,6 @@
 <aside id="sidebar">
+	<p>Il y a actuellement <?php $result = count_users();
+echo $result['total_users'];?> inscrits</p>
 	<?php if (! dynamic_sidebar('primary-widget-area')) { ?>
 
 	<?php } ?>
